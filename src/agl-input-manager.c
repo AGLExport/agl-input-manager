045f9e6bb0bd95e9f382d4a5eed9edad6b8d4ee4
Naoto Yamaguchi <naoto.yamaguchi@aisin.co.jp>
2025-10-04 21:35:54 +0900

    Initial commit
    Signed-off-by: Naoto Yamaguchi <naoto.yamaguchi@aisin.co.jp>

---
+++ src/agl-input-manager.c
@@ -0,0 +1,25 @@
+/**
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * @file	agl-input-manager.c
+ * @brief	main source file for input management daemon for container host.
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+/**
+ * The main function for agl-input-manager.
+ */
+int main(int argc, char *argv[])
+{
+	int ret = -1;
+	int result = 0;
+
+do_return:
+	return result;
+}

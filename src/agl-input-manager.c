9327baaa691ea2d4be59969f89bec47425062682
Naoto Yamaguchi <naoto.yamaguchi@aisin.co.jp>
2025-10-22 08:27:26 +0900

    Add virtual device creation and event forward feature
    The agl-input-manager role is input device management in a container environment.
    It needs to;
     Detect a new device.
     Create a virtual device to forward input events to the guest container.
     Redirect input events from physical devices to virtual devices.
    
    This patch is a part of virtual device creation and event forwarding.
    
    Signed-off-by: Naoto Yamaguchi <naoto.yamaguchi@aisin.co.jp>

--- src/agl-input-manager.c
+++ src/agl-input-manager.c
@@ -16,12 +16,17 @@
 #include <systemd/sd-event.h>
 
 #include "device-udev.h"
+#include "device-input.h"
+
 
 static int device_udev_listener_handler(device_udev_info_t *pdui, void* puserdata)
 {
+	device_input_t *pdi = (device_input_t *)puserdata;
+
 	#ifdef _PRINTF_DEBUG_
-	(void) fprintf(stdout,"udi: action=%s devnode=%s subsystem=%s\n", pdui->action, pdui->dev_node, pdui->sub_system);
+	//(void) fprintf(stdout,"udi: action=%s devnode=%s subsystem=%s\n", pdui->action, pdui->dev_node, pdui->sub_system);
 	#endif
+	device_input_add_new_device(pdui->dev_node, pdi);
 
 	return 0;
 }
@@ -34,9 +39,11 @@ int main(int argc, char *argv[])
 	int ret = -1, result = 0;
 	sd_event *event = NULL;
 	dynamic_device_udev_t *pddu = NULL;
+	device_input_t *ppdi = NULL;
 
 	ret = sd_event_default(&event);
 	if (ret < 0) {
+		result = -1;
 		goto finish;
 	}
 
@@ -49,8 +56,15 @@ int main(int argc, char *argv[])
 	}
 	*/
 
-	ret = device_udev_setup(&pddu, event, device_udev_listener_handler, NULL);
+	ret = device_input_setup(&ppdi, event);
 	if (ret < 0) {
+		result = -1;
+		goto finish;
+	}
+
+	ret = device_udev_setup(&pddu, event, device_udev_listener_handler, (void*)ppdi);
+	if (ret < 0) {
+		result = -1;
 		goto finish;
 	}
 
@@ -66,14 +80,8 @@ int main(int argc, char *argv[])
 		"READY=1\n"
 		"STATUS=Daemon startup completed, processing events.");
 
-	#ifdef _PRINTF_DEBUG_
-	(void) fprintf(stdout,"agl-input-manager: do device scan\n");
-	#endif
 	device_udev_scan(pddu);
 
-	#ifdef _PRINTF_DEBUG_
-	(void) fprintf(stdout,"agl-input-manager: start event loop\n");
-	#endif
 	ret = sd_event_loop(event);
 	if (ret < 0) {
 		result = ret;
